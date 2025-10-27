91f44838846eeb997f14147924981e353f468a5b
Ermolai <ermolay9@gmail.com>
2025-10-27 21:39:32 +0200

    add animations

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -7,7 +7,7 @@ function Card({ className, ...props }: React.ComponentProps<'div'>) {
     <div
       data-slot="card"
       className={cn(
-        'bg-card text-card-foreground flex flex-col gap-6 rounded-xl border py-6 shadow-sm',
+        'bg-card text-card-foreground flex flex-col gap-6 rounded-xl border py-6 shadow-sm dark:shadow-[0_4px_6px_-1px_rgba(255,255,255,0.1),0_2px_4px_-1px_rgba(255,255,255,0.06)]',
         className,
       )}
       {...props}
